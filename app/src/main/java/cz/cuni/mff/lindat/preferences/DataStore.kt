54e6f04837bae6faf8c6918509f7e53d7d5c9e88
tomas.krabac <tomas.krabac@gmail.com>
2022-04-06 20:30:40 +0200

    basic data storage for key value data

---
+++ app/src/main/java/cz/cuni/mff/lindat/preferences/DataStore.kt
@@ -0,0 +1,43 @@
+package cz.cuni.mff.lindat.preferences
+
+import android.content.Context
+import androidx.datastore.core.DataStore
+import androidx.datastore.preferences.core.Preferences
+import androidx.datastore.preferences.core.booleanPreferencesKey
+import androidx.datastore.preferences.core.edit
+import androidx.datastore.preferences.preferencesDataStore
+import kotlinx.coroutines.flow.map
+
+/**
+ * @author Tomas Krabac
+ */
+
+class UserDataStore(private val context: Context): IUserDataStore {
+
+    private val Context.userDataStore: DataStore<Preferences> by preferencesDataStore(name = "user_settings")
+
+    companion object {
+        val HAS_FINISHED_ONBOARDING = booleanPreferencesKey("HAS_FINISHED_ONBOARDING")
+        val AGREE_WITH_DATA_COLLECTION = booleanPreferencesKey("AGREE_WITH_DATA_COLLECTION")
+    }
+
+    override suspend fun setFinishedOnboarding() {
+        context.userDataStore.edit {
+            it[HAS_FINISHED_ONBOARDING] = true
+        }
+    }
+
+    override fun hasFinishedOnboarding() = context.userDataStore.data.map {
+        it[HAS_FINISHED_ONBOARDING] ?: false
+    }
+
+    override suspend fun saveAgreementDataCollection(agree: Boolean) {
+        context.userDataStore.edit {
+            it[AGREE_WITH_DATA_COLLECTION] = agree
+        }
+    }
+
+    override fun agreeWithDataCollection() = context.userDataStore.data.map {
+        it[AGREE_WITH_DATA_COLLECTION] ?: false
+    }
+}
