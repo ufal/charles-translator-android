ff8180144a335049864f8575d314dcd448d18294
tomas.krabac <tomas.krabac@gmail.com>
2022-04-10 12:52:44 +0200

    fixed czech TTS #1

--- app/src/main/java/cz/cuni/mff/ufal/translator/ui/translations/models/Language.kt
+++ app/src/main/java/cz/cuni/mff/ufal/translator/ui/translations/models/Language.kt
@@ -6,6 +6,6 @@ import java.util.*
  * @author Tomas Krabac
  */
 enum class Language(val code: String, val locale: Locale) {
-    Czech("cs", Locale("cs")),
+    Czech("cs", Locale("cs-CZ")),
     Ukrainian("uk", Locale("ru_UA"))
 }
