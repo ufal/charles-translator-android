cd3ea79ecfea62bdf0033b4874eb175c37dd9fcb
tomas.krabac <tomas.krabac@gmail.com>
2022-04-12 17:59:04 +0200

    fix cz locale

--- app/src/main/java/cz/cuni/mff/ufal/translator/ui/translations/models/Language.kt
+++ app/src/main/java/cz/cuni/mff/ufal/translator/ui/translations/models/Language.kt
@@ -6,6 +6,6 @@ import java.util.*
  * @author Tomas Krabac
  */
 enum class Language(val code: String, val locale: Locale) {
-    Czech("cs", Locale("cs-CZ")),
+    Czech("cs", Locale("cs_CZ")),
     Ukrainian("uk", Locale("uk_UA"))
 }
