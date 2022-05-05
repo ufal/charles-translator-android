0fb3c3544dcbd359c5f8ea476d2d9a907449cb2e
tomas.krabac <tomas.krabac@gmail.com>
2022-05-05 20:19:39 +0200

    fix darkmode analytics

--- app/src/main/java/cz/cuni/mff/ufal/translator/interactors/analytics/Analytics.kt
+++ app/src/main/java/cz/cuni/mff/ufal/translator/interactors/analytics/Analytics.kt
@@ -1,6 +1,7 @@
 package cz.cuni.mff.ufal.translator.interactors.analytics
 
 import android.content.Context
+import android.content.res.Configuration
 import android.speech.SpeechRecognizer
 import android.util.Log
 import com.google.firebase.analytics.ktx.analytics
@@ -12,8 +13,10 @@ import cz.cuni.mff.ufal.translator.extensions.logD
 import cz.cuni.mff.ufal.translator.interactors.analytics.events.TranslateEvent
 import cz.cuni.mff.ufal.translator.interactors.crashlytics.FirebaseConstants
 import cz.cuni.mff.ufal.translator.interactors.preferences.IUserDataStore
+import cz.cuni.mff.ufal.translator.interactors.preferences.data.DarkModeSetting
 import kotlinx.coroutines.flow.first
 import kotlinx.coroutines.runBlocking
+import java.security.AccessController.getContext
 import java.util.*
 import javax.inject.Inject
 
@@ -31,20 +34,27 @@ class Analytics @Inject constructor(
     override fun setBaseInfo(context: Context) {
         val isRecognitionAvailable = SpeechRecognizer.isRecognitionAvailable(context)
         val agreeWithDataCollection = runBlocking { userDataStore.agreeWithDataCollection.first() }
-        val darkMode = runBlocking { userDataStore.darkModeSetting.first() }
+        val darkModeSettings = runBlocking { userDataStore.darkModeSetting.first() }
+        val darkModeValue = when(darkModeSettings){
+            DarkModeSetting.System -> context.resources.configuration.uiMode.and(Configuration.UI_MODE_NIGHT_MASK) == Configuration.UI_MODE_NIGHT_YES
+            DarkModeSetting.Enabled -> true
+            DarkModeSetting.Disabled -> false
+        }
 
         Firebase.crashlytics.setCustomKeys {
             key(FirebaseConstants.PARAM_USER_LOCALE, language)
             key(FirebaseConstants.PARAM_IS_RECOGNITION_AVAILABLE, isRecognitionAvailable)
             key(FirebaseConstants.AGREE_WITH_DATA_COLLECTION, agreeWithDataCollection)
-            key(FirebaseConstants.DARK_MODE, darkMode.key)
+            key(FirebaseConstants.DARK_MODE_SETTINGS, darkModeSettings.key)
+            key(FirebaseConstants.DARK_MODE_VALUE, darkModeValue.toString())
         }
 
         Firebase.analytics.apply {
             setUserProperty(FirebaseConstants.PARAM_USER_LOCALE, language)
             setUserProperty(FirebaseConstants.PARAM_IS_RECOGNITION_AVAILABLE, isRecognitionAvailable.toString())
             setUserProperty(FirebaseConstants.AGREE_WITH_DATA_COLLECTION, agreeWithDataCollection.toString())
-            setUserProperty(FirebaseConstants.DARK_MODE, darkMode.key)
+            setUserProperty(FirebaseConstants.DARK_MODE_SETTINGS, darkModeSettings.key)
+            setUserProperty(FirebaseConstants.DARK_MODE_VALUE, darkModeValue.toString())
         }
     }
 
--- app/src/main/java/cz/cuni/mff/ufal/translator/interactors/crashlytics/FirebaseConstants.kt
+++ app/src/main/java/cz/cuni/mff/ufal/translator/interactors/crashlytics/FirebaseConstants.kt
@@ -8,5 +8,6 @@ object FirebaseConstants {
     const val PARAM_USER_LOCALE = "locale"
     const val PARAM_IS_RECOGNITION_AVAILABLE = "is_recognition_available"
     const val AGREE_WITH_DATA_COLLECTION = "agree_data_collection"
-    const val DARK_MODE = "dark_mode"
+    const val DARK_MODE_SETTINGS = "dark_mode_settings"
+    const val DARK_MODE_VALUE = "dark_mode_value"
 }
